1863430b3e5f2785bb9c4ebeb6c307f0f0286598
Hugo Morin <dofus-91510@hotmail.fr>
2019-11-14 16:11:22 +0100

    je sais pas pourquoi mais ils on été modifié

--- View/ViewSignUpGood.php
+++ View/ViewSignUpGood.php
@@ -6,7 +6,7 @@
     <body>
         <?php 
         echo("Tu as reussi a t'inscrire, ton nom d'utilisateur est : ".$_GET['ndu']." Et ton mot de passe : ".$_GET['mdp']."<br>
-        <a href='..\Index.php'><input type='button' value='Retour accueil'></a>");
+        <a href='../Index.php'><input type='button' value='Retour accueil'></a>");
         ?>
     </body>
 </html>
