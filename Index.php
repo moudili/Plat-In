cf9b3528677c06d2c23f3f1a630b38ee4c456e28
Hugo Morin <dofus-91510@hotmail.fr>
2019-11-14 18:22:10 +0100

    chatte

--- Index.php
+++ Index.php
@@ -9,27 +9,6 @@
             CheckSesion();
             CheckCo();
 
-            /*$Bdd = new PDO("mysql:host=localhost;dbname=plat_in","root","");
-            $Query = $Bdd->query("SELECT user, password FROM users");
-
-            $Lignes = $Bdd->query("SELECT COUNT(ID_user) FROM users"); // Compter le nombre de lignes dans une table en fonction des ID.
-            $Taille = $Lignes->fetch();
-            $pass='a';
-
-            for($i = 0 ; $i<$Taille[0] ; $i++) 
-            {
-                $Reponse = $Query->fetch();
-                echo($Reponse[0]."        mdp : ".$Reponse[1]);
-
-                if (password_verify($pass, $Reponse[1]))
-                {
-                    echo "Mot de passe correct";
-                }
-                else
-                {
-                    echo "Mot de passe incorrect";
-                };
-            };*/
         ?>
     </body>
 </html>        
