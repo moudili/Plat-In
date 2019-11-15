470bc4d20cce4a1f57f8e75213d36dfd2f20db2a
Jean PHILIPP <jpp@MacBook-Pro-de-Jean.local>
2019-11-15 13:37:16 +0100

    suppr

--- Model/ModelLogOut.php
+++
@@ -1,8 +0,0 @@
-<?php
-     /* deconnecte un utilisateur */
-     $ID = $_SESSION['id'];
-     $Bdd = new PDO("mysql:host=localhost;dbname=Plat_In","root","");
-     $Req = $Bdd -> prepare("UPDATE `users` SET `connection` = 'dc' WHERE `users`.`ID_user` = :id ;");
-     $Req->bindParam(':id',$ID,PDO::PARAM_STR);
-     $Req->execute();
-?>
