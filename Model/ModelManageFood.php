1dac02c1edc082c02bfb48f965cc08d6e7ea2042
Hugo Morin <dofus-91510@hotmail.fr>
2019-12-02 19:05:40 +0100

    Food pareil que Origin (code similaire)

--- Model/ModelManageFood.php
+++ Model/ModelManageFood.php
@@ -1,41 +1,138 @@
 <?php
-    function BDDChange($org)
+    function Foods()
     {
-
-        if(isset($_GET['ModifierAliment'])==TRUE)
+        if(empty($_GET['Request']))
         {
-            require('Model/ModelNewPDO.php');
-            $Req = $Bdd->prepare("UPDATE foods SET food_name=:food_name WHERE ID_food=:ID");
-            $Req->bindParam(':food_name', $org, PDO::PARAM_STR);
-            $Req->bindParam(':ID', $_GET['ModifierAliment'], PDO::PARAM_STR);
-            $Req->execute();
-        }
-        elseif(isset($_GET['SupprimerAliment'])==TRUE)
-        {
-            require('Model/ModelNewPDO.php');
-            $Req = $Bdd->prepare("DELETE FROM foods WHERE ID_food=:ID");
-            $Req->bindParam(':ID', $_GET['SupprimerAliment'], PDO::PARAM_STR);
-            $Req->execute();
-        }
-        elseif(isset($_GET['TextAddFood'])==TRUE)
-        {
-            require('Model/ModelNewPDO.php');
-            $Req = $Bdd->prepare("INSERT INTO foods (ID_food, food_name)  VALUES (NULL, :FoodName)");
-            $Req->bindParam(':FoodName', $org, PDO::PARAM_STR);
-            $Req->execute();
-        }
-    }
-
-    function BDDFood()
-    {
-        if(empty($_GET['Request']) == TRUE)
-        {
-            require('Model/ModelNewPDO.php');
-            $Req = $Bdd->prepare("SELECT ID_food,food_name FROM foods");
-            $Req->execute();
-            $Aliments = $Req->fetchAll();
+            require("Model/ModelNewPDO.php");
+            $Req = $Bdd -> prepare("SELECT ID_food,food_name FROM foods ORDER BY food_name");
+            $Req -> execute();
+            $Foods = array(array(),array());
+            while($n = $Req -> fetch())
+            {
+                array_push($Foods[0], $n[0]);
+                array_push($Foods[1], $n[1]);               
+            }
             
-            return $Aliments;
+            return $Foods;
         }
     }
+
+    function CheckForm ($Ali) 
+    {
+        if($_GET['Request'] == "Ajouter")
+        {
+            if(!empty($_GET['Ali'])
+            && strlen($_GET['Ali']) > 2
+            && strlen($_GET['Ali']) < 40
+            && preg_match("#^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ '._-]+$#", $_GET['Ali'])
+            )
+            {
+                require("Model/ModelNewPDO.php");
+                $Req = $Bdd -> prepare("SELECT count(ID_food) FROM foods WHERE food_name LIKE :food");
+                $Req -> bindParam(':food',$Ali,PDO::PARAM_STR);
+                $Req -> execute();
+                $n = $Req -> fetch();
+                $CheckForm = $n[0];
+
+                if($CheckForm == 0)
+                {
+                    $Req = $Bdd -> prepare("INSERT INTO `foods` (`ID_food`, `food_name`) VALUES (NULL, :food);");
+                    $Req -> bindParam(':food',$Ali,PDO::PARAM_STR);
+                    $Req -> execute();                    
+
+                }
+
+                return $CheckForm;
+
+            }
+        }
+    }
+
+    function DeletFood() 
+    {
+        if($_GET['Request'] == "Supprimer")
+        {
+            if(!empty($_GET['Answer']))
+            {
+                $Id = $_GET['id'];
+                require("Model/ModelNewPDO.php");
+                $Req = $Bdd -> prepare("DELETE FROM `foods` WHERE `foods`.`ID_food` = :id");
+                $Req -> bindParam(':id',$Id,PDO::PARAM_STR);
+                $Req -> execute();       
+
+            }
+        }
+    }
+
+    function ModifFood($Ali)
+    {
+        if($_GET['Request'] == "Modifier cet aliment")
+        {
+            if(!empty($_GET['Ali'])
+            && strlen($_GET['Ali']) > 2
+            && strlen($_GET['Ali']) < 40
+            && preg_match("#^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ '._-]+$#", $_GET['Ali'])
+            )
+            {
+                $Id = $_GET['id'];
+                require("Model/ModelNewPDO.php");
+                $Req = $Bdd -> prepare("SELECT count(ID_food) FROM foods WHERE food_name LIKE :food");
+                $Req -> bindParam(':food',$Ali,PDO::PARAM_STR);
+                //$Req -> bindParam(':id',$Id,PDO::PARAM_STR);
+                $Req -> execute();
+                $n = $Req -> fetch();
+                $CheckForm = $n[0];
+
+                if($CheckForm == 0)
+                {
+                    $Req = $Bdd -> prepare("UPDATE `foods` SET `food_name` = :food WHERE `ID_food` = :id");
+                    $Req -> bindParam(':food',$Ali,PDO::PARAM_STR);
+                    $Req -> bindParam(':id',$Id,PDO::PARAM_STR);
+                    $Req -> execute();                    
+
+                }
+
+                return $CheckForm;
+            }
+        }
+    }
+ 
+    function SearchFood($Ali)
+    {
+        if($_GET['Request'] == "Search")
+        {
+            require("Model/ModelNewPDO.php");
+            if(empty($_GET['Ali']))
+            {
+                $Req = $Bdd -> prepare("SELECT ID_food,food_name FROM foods ORDER BY food_name");
+                $Req -> execute();
+                $Foods = array(array(),array());
+                while($n = $Req -> fetch())
+                {
+                    array_push($Foods[0], $n[0]);
+                    array_push($Foods[1], $n[1]);               
+                }
+            }
+            else
+            {
+                $Req = $Bdd -> prepare('SELECT ID_food,food_name FROM foods WHERE food_name LIKE "%'.$Ali.'%" ORDER BY food_name');
+                $Req -> execute();
+                $Foods = array(array(),array());
+                if($Req->rowCount() > 0)
+                {
+                    while($n = $Req -> fetch())
+                    {
+                        array_push($Foods[0], $n[0]);
+                        array_push($Foods[1], $n[1]);               
+                    }
+                }
+                else
+                {
+                    $Foods = false;
+                }
+            }
+            return $Foods;
+        }
+    }
+
 ?>
