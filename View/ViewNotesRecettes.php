d3a88e73864adf5d990d7f453ae0f388afc00711
Hugo Morin <dofus-91510@hotmail.fr>
2020-01-08 14:35:19 +0100

    modif note recette admin*

--- View/ViewNotesRecettes.php
+++ View/ViewNotesRecettes.php
@@ -14,11 +14,7 @@
             <input type='hidden' name='page' value='Notes recettes'>
             </form>
             <table border=4 align='center'>
-            <p>Notes des recettes</br></br>
-            <form action='Index.php' method='get'>
-            <input type='hidden' name='page' value='Notes recettes'>
-            <input type='submit' name='Request' value='Ajouter une note'>
-            </form>");
+            <p>Notes des recettes</br></br>");
             for ($i=0;$i<count($Note[0]);$i++)
             {
                 echo("<form action='Index.php' method='get'>
@@ -59,39 +55,6 @@
             <input type='hidden' name='page' value='Notes recettes'>
             <input type='submit' value='Retour'>
             </form>");
-        }  
-        else if ($_GET['Request']=="Ajouter une note")
-        {
-            if (empty($_GET['RequestAdd']))
-            {
-                echo("<div class='text-center mt-5'>
-                <form action='Index.php' method='get'>
-                <input type='hidden' name='page' value='Notes recettes'>
-                <input type='hidden' name='Request' value='Ajouter une note'>
-                <select name='stars'>
-                    <option value=''>--Choisissez une note--</option>
-                    <option value='1'>1 étoile</option>
-                    <option value='2'>2 étoiles</option>
-                    <option value='3'>3 étoiles</option>
-                    <option value='4'>4 étoiles</option>
-                    <option value='5'>5 étoiles</option>
-                </select>
-                <input type='submit' name='RequestAdd' value='Valider'>
-                </form>
-                <form action='Index.php' method='get'>
-                <input type='hidden' name='page' value='Notes recettes'>
-                <input type='submit' value='Retour'>
-                </form>");
-            }
-            else if ($_GET['RequestAdd']=='Valider')
-            {
-                echo("<div class='text-center mt-5'>
-                Vous avez bien ajouté cette note à la Base de Donnée.<br></br>
-                <form action='Index.php' method='get'>
-                <input type='hidden' name='page' value='Notes recettes'>
-                <input type='submit' value='Retour'>
-                </form>");
-            }
         }     
 
         ?>
