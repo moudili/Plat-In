62b5e9b9ca1ef769c2ec9361d99f53f65cd39556
Hugo Morin <dofus-91510@hotmail.fr>
2020-01-20 09:12:15 +0100

    ajout event

--- Model/ModelEvent.php
+++ Model/ModelEvent.php
@@ -1,9 +1,9 @@
 <?php
 
     function SelectEvent()
-    {
+    { 
         require('Model\ModelNewPDO.php');
-        $Req = $Bdd -> prepare("SELECT service_name FROM dishes");
+        $Req = $Bdd -> prepare("SELECT name_m FROM meals");
         $Req -> execute();
         $Events=array();
         while($n = $Req -> fetch())
@@ -33,5 +33,38 @@
             array_push($Recipes[2], $n[2]);
         }
         return $Recipes;
+    }
+
+    function SelectUser()
+    {
+        require('Model\ModelNewPDO.php');
+        $Req = $Bdd -> prepare("SELECT ID_user,user,status_u
+        FROM users 
+        WHERE ID_user NOT LIKE :id
+        AND status_u NOT LIKE 'admin'
+        ORDER BY user");
+        $Req -> bindParam(':id',$_SESSION['id'],PDO::PARAM_STR);
+        $Req -> execute();
+        $Users = array(array(),array(),array());
+        while($n = $Req -> fetch())
+        {
+            array_push($Users[0], $n[0]);
+            array_push($Users[1], $n[1]);
+            array_push($Users[2], $n[2]);
+        }
+        return $Users;
+    }
+
+    function InsertEvent()
+    {
+        if (!empty($_GET['Request']) AND $_GET['Request'] == "Confirmer")
+        {
+            echo("CHATE");
+            require('Model\ModelNewPDO.php');
+            $Req = $Bdd -> prepare("INSERT INTO `meals` (`name_m`, `date_m`) VALUES (:name_e, :date_e);");
+            $Req -> bindParam(':name_e',$_GET['Name'],PDO::PARAM_STR);
+            $Req -> bindParam(':date_e',$_GET['date'],PDO::PARAM_INT);
+            $Req -> execute();
+        }
     }
 ?>
--- View/ViewEvent.php
+++ View/ViewEvent.php
@@ -6,7 +6,7 @@
     
     <body>
         <?php
-            
+             
             require("View/ViewBanner.php");     
         
             if (empty($_GET['event']))
@@ -18,6 +18,8 @@
                     Venez vite en créer ci-dessous !<br></br>
                     <form action='Index.php' method='get'>
                         <input type='submit' name='event' value='Ajouter un evenement'>
+                        <input type='hidden' name='MenuRecipe' value=1>
+                        <input type='hidden' name='MenuUser' value=1>
                         <input type='hidden' name='page' value='Evènement'>
                     </form>
                     </div>");
@@ -27,44 +29,209 @@
                     echo("<div class='text-center mt-5'>
                     <form action='Index.php' method='get'>
                         <input type='submit' name='event' value='Ajouter un evenement'>
+                        <input type='hidden' name='MenuRecipe' value=1>
+                        <input type='hidden' name='MenuUser' value=1>
                         <input type='hidden' name='page' value='Evènement'>
                     </form>
-                    Evenement :
+                    <p>Evenement :</p>
                     ");
                     for ($i=0;$i<count($Events);$i++)
                     {
-                        echo($Events[$i]."<br>");
+                        echo($Events[$i]."<br><br>");
                     }
                 }
             }
             else if ($_GET['event']=='Ajouter un evenement')
             {
-                echo("<div class='text-center mt-5'>
-                <h2>Ajouter un évènement</h2>
-                <br><br>
-                <p>Personne à inviter à cette évènement : </p>
-                <p>Recette disponible : 
-                <select name='recette'>
-                ");
-                for ($i=0;$i<count($Recipes[0]);$i++)
+                if (empty($_GET['Request']) AND empty($_GET['RequestRecipe']) AND empty($_GET['RequestUser']))
                 {
-                    echo("<option value='".$Recipes[2]."'>".$Recipes[0][$i]."</option>");
+                    echo("<div class='text-center mt-5'>
+                    <form action='Index.php' method='get'>
+                    <h2>Ajouter un évènement</h2>
+                    <br><br>
+                    <p>Titre de l'évènement : <input type='text' name='Name' value=''></p>
+                    <p>Personne à inviter à cette évènement : 
+                    <select name='utilisateur0'>");
+                    for ($i=0;$i<count($Users[0]);$i++)
+                    {
+                        echo("<option value='".$Users[0][$i]."'>".$Users[1][$i]."</option><br>");
+                    }
+                    echo("</select>
+                    <br>
+                    <input type='hidden' name='MenuUser' value='".$_GET['MenuUser']."'>
+                    <input type='submit' name='RequestUser' value='+'>
+                    </p>
+                    <p>Recette disponible : 
+                    <select name='recette0'>
+                    ");
+                    for ($i=0;$i<count($Recipes[0]);$i++)
+                    {
+                        echo("<option value='".$Recipes[2][$i]."'>".$Recipes[0][$i]."</option>");
+                    }
+                    echo("</select></p>
+                    <p>Ceci est <select name='service0'>
+                    <option value='entrée'>une entrée</option>
+                    <option value='plat'>un plat</option>
+                    <option value='dessert'>un dessert</option>
+                    </select></p>
+                    <input type='hidden' name='MenuRecipe' value='".$_GET['MenuRecipe']."'>
+                    <p><input type='submit' name='RequestRecipe' value='+'></p>
+                    <p><input type='date' name='date'><input type='time' name='time'></p>
+                    <p><TEXTAREA name='description' rows=4 cols=40></TEXTAREA></p>
+                    <input type='submit' name='Request' value='Confirmer'>
+                    <input type='hidden' name='event' value='Ajouter un evenement'>
+                    <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    <form action='Index.php' method='get'>
+                        <input type='submit' value='Retour'>
+                        <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    </div>");
                 }
-                echo("</select></p>
-                <p>Ceci est <select name='service'>
-                <option value='entrée'>une entrée</option>
-                <option value='plat'>un plat</option>
-                <option value='dessert'>un dessert</option>
-                </select></p>
-                <form action='Index.php' method='get'>
-                    <input type='submit' name='event' value='Confirmer'>
+                else if (!empty($_GET['RequestRecipe']) AND ($_GET['RequestRecipe'] == "+"
+                || $_GET['RequestRecipe'] == "-"))
+                {
+                    echo("<div class='text-center mt-5'>
+                    <form action='Index.php' method='get'>
+                    <h2>Ajouter un évènement</h2>
+                    <br><br>
+                    <p>Titre de l'évènement : <input type='text' name='Name' value='".$_GET['Name']."'></p>
+                    <p>");
+                    for($j = 0 ; $j < $_GET['MenuUser'] ; $j++ )
+                    {
+                        echo("Personne à inviter à cette évènement : 
+                        <select name='utilisateur0'>");
+                        for ($i=0;$i<count($Users[0]);$i++)
+                        {
+                            echo("<option value='".$Users[0][$i]."'>".$Users[1][$i]."</option>");
+                        }
+                        echo("</select></p>");
+                    }
+                    echo("<input type='hidden' name='MenuUser' value='".$_GET['MenuUser']."'>
+                    </p><input type='submit' name='RequestUser' value='+'>");
+                    
+                    if($_GET['MenuUser'] > 1 )
+                    {
+                        echo("<input type='submit' name='RequestUser' value='-'>");
+                    }
+
+                    echo("</p>
+                    <p>
+                    ");
+                    for($j = 0 ; $j < $MenuRecipe ; $j++ )
+                    {
+                        echo("Recette disponible : ");
+                        echo("<select name='recette".$j."'>");
+                        for ($i=0;$i<count($Recipes[0]);$i++)
+                        {
+                            echo("<option value='".$Recipes[2][$i]."'>".$Recipes[0][$i]."</option>");
+                        }
+                        echo("</select></p>
+                        <p>Ceci est <select name='service".$j."'>
+                        <option value='entrée'>une entrée</option>
+                        <option value='plat'>un plat</option>
+                        <option value='dessert'>un dessert</option>
+                        </select></p>
+                        ");
+                    }
+                    echo("
+                    <input type='hidden' name='MenuRecipe' value=".$MenuRecipe.">
+                    <p><input type='submit' name='RequestRecipe' value='+'>");
+
+                    if( $MenuRecipe > 1 )
+                    {
+                        echo("<input type='submit' name='RequestRecipe' value='-'>");
+                    }
+
+                    echo("</p>
+                    <p><input type='date' name='date'><input type='time' name='time'></p>
+                    <input type='submit' name='Request' value='Confirmer'>
+                    <input type='hidden' name='event' value='Ajouter un evenement'>
                     <input type='hidden' name='page' value='Evènement'>
-                </form>
-                <form action='Index.php' method='get'>
-                    <input type='submit' value='Retour'>
+                    </form>
+                    <form action='Index.php' method='get'>
+                        <input type='submit' value='Retour'>
+                        <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    </div>");
+                }
+                else if (!empty($_GET['RequestUser']) AND ($_GET['RequestUser'] == "+"
+                OR $_GET['RequestUser'] == "-"))
+                {
+                    echo("<div class='text-center mt-5'>
+                    <form action='Index.php' method='get'>
+                    <h2>Ajouter un évènement</h2>
+                    <br><br>
+                    <p>Titre de l'évènement : <input type='text' name='Name' value='".$_GET['Name']."'></p>
+                    <p>");
+                    for($j = 0 ; $j < $MenuUser ; $j++ )
+                    {
+                        echo("Personne à inviter à cette évènement : 
+                        <select name='utilisateur0'>");
+                        for ($i=0;$i<count($Users[0]);$i++)
+                        {
+                            echo("<option value='".$Users[0][$i]."'>".$Users[1][$i]."</option>");
+                        }
+                        echo("</select></p>");
+                    }
+                    echo("<input type='hidden' name='MenuUser' value='".$MenuUser."'>
+                    <input type='submit' name='RequestUser' value='+'>");
+                    
+                    if( $MenuUser > 1 )
+                    {
+                        echo("<input type='submit' name='RequestUser' value='-'>");
+                    }
+
+                    echo("</p>");
+                    for($j = 0 ; $j < $_GET['MenuRecipe'] ; $j++ )
+                    {
+                        echo("Recette disponible : ");
+                        echo("<p><select name='recette".$j."'>");
+                        for ($i=0;$i<count($Recipes[0]);$i++)
+                        {
+                            echo("<option value='".$Recipes[2][$i]."'>".$Recipes[0][$i]."</option>");
+                        }
+                        echo("</select></p>
+                        <p>Ceci est <select name='service".$j."'>
+                        <option value='entrée'>une entrée</option>
+                        <option value='plat'>un plat</option>
+                        <option value='dessert'>un dessert</option>
+                        </select></p>
+                        ");
+                    }
+                    echo("<p>
                     <input type='hidden' name='page' value='Evènement'>
-                </form>
-                </div>");
+                    <input type='hidden' name='event' value='Ajouter un evenement'>
+                    <input type='hidden' name='MenuRecipe' value=".$_GET['MenuRecipe'].">
+                    <input type='submit' name='RequestRecipe' value='+'>");
+                    if($_GET['MenuRecipe'] > 1 )
+                    {
+                        echo("<input type='submit' name='RequestRecipe' value='-'>");
+                    }
+
+                    echo("</p>
+                    <p><input type='date' name='date'><input type='time' name='time'></p>
+                    <input type='submit' name='Request' value='Confirmer'>
+                    <input type='hidden' name='event' value='Ajouter un evenement'>
+                    <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    <form action='Index.php' method='get'>
+                        <input type='submit' value='Retour'>
+                        <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    </div>");
+                }
+                else if ($_GET['Request'] == "Confirmer")
+                {
+                    echo("<div class='text-center mt-5'>
+                    Vous avez bien créé votre évènement !
+                    <br><br>
+                    <form action='Index.php' method='get'>
+                        <input type='submit' value='Retour'>
+                        <input type='hidden' name='page' value='Evènement'>
+                    </form>
+                    </div>");
+                }
             }
         ?> 
     </body>
