c2ae7b70cdb087c13bcc8ceb55460825d24e28ba
Leo <guimard@intechinfo.fr>
2020-01-31 09:46:54 +0100

    oui

--- View/ViewHome.php
+++ View/ViewHome.php
@@ -9,8 +9,20 @@ require("View/ViewBanner.php");
         <link rel="stylesheet" href="Css/Bootstrap/PersonalCss.css">
         <link rel="stylesheet" href="Css/Bootstrap/background.css">
     <title>Plat'In</title>
+    <style>
+    .center{
+    font-size: 30px;
+    }
+    </style>
 </head>
 <body class='backgroundhat'>
-    
+<div class = "center">
+<h1>Plat&#39;In</h1>
+A vous qui aimez manger, partager un bon repas, cuisiner avec amour pour vos proches,<br>
+tester plein de recettes étonnantes, faites part de vos avis et de vos trouvailles. Et puis vous<br>
+qui avez faim, tout simplement.<br>
+
+Plat&#39;In est fait pour vous. Rejoignez la communauté des gourmands !<br>
+<div>
 </body>
 </html>
